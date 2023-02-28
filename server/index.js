299ebd69f17bcb286576f6c57493f5b094292e6b
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2023-02-28 01:32:21 -0800

    connection created but, status is no connection

--- server/index.js
+++ server/index.js
@@ -3,8 +3,23 @@
 
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 const hostname = "localhost"
+
+const DB = 'mongodb+srv://Tehniyat_Fatima_Fareed:yudc9411@cluster0.4zyocie.mongodb.net/project1?retryWrites=true&w=majority'
+
+mongoose.connect(DB,{
+
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false 
+
+}).then(() =>{
+    console.log(`connection sucessful`)
+}).catch((err) => console.log(`no connection`))
+
 
 // lets create first route
 app.get("/",(req,res) =>{
