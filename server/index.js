21e316d97fb7cf33868a913e22ed1f3c7d5028fd
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2023-03-01 04:30:20 -0800

    set config file for securing

--- server/index.js
+++ server/index.js
@@ -3,22 +3,16 @@
 
 const express = require("express")
 const app = express()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const port = 3000
 const hostname = "localhost"
 
-const DB = 'mongodb+srv://Tehniyat_Fatima_Fareed:yudc9411@cluster0.4zyocie.mongodb.net/project1?retryWrites=true&w=majority'
+dotenv.config({path: './config.env'})
+require('./db/conn');
 
-mongoose.connect(DB,{
+const PORT = 'process.env.PORT'
 
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false 
 
-}).then(() =>{
-    console.log(`connection sucessful`)
-}).catch((err) => console.log(`no connection`))
 
 
 // lets create first route
@@ -45,6 +39,6 @@ app.get("/contact",(req,res) =>{
 
 
 // here our app will listen
-app.listen(port,() =>{
-    console.log(`your app is runing at  http://${hostname}:${port}`)
+app.listen(PORT,() =>{
+    console.log(`your app is runing at  http://${hostname}:${PORT}`)
 })
